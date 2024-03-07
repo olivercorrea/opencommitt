69139c0afc02066d6cf95b8148b2bb147f8afd07
jomaol <olivercorreacabana@outlook.es>
2024-03-07 18:55:40 -0500

    Refactored server.ts, changed port variable name and added support for dynamic ports. Added new while.js file with console outputs and a loop to print array numbers.

---
+++ while.js
@@ -0,0 +1,10 @@
+console.log('prueba de uso de while con array');
+console.log();
+
+let numeros = [1, 2, 3, 4, 5 , 6, 7, 8];
+let i = 0
+
+while (numeros[i] <= numeros.length){
+    console.log(numeros[i]);
+    i++;
+}
